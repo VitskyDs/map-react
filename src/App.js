fb7acba06b2366d404b2fd62365427103653d198
Vitsky <vitskyds@gmail.com>
2018-07-31 20:52:11 +0300

    Feat: filter results by category

--- src/App.js
+++ src/App.js
@@ -1,81 +1,84 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Map from './Map.js'
 import './App.css';
 
 class App extends Component {
   state = {
-    locations: [{
+    locations: [
+      {
         name: 'Mahane Yehuda',
-        id: '',
+        id: '1',
         lat: 31.785556,
-        lng: 35.2100333
-      },
-      {
+        lng: 35.2100333,
+        category: 'secular'
+      }, {
         name: 'Blumfield Garden',
-        id: '',
+        id: '2',
         lat: 31.772415,
-        lng: 35.2211988
-      },
-      {
+        lng: 35.2211988,
+        category: 'hangout'
+      }, {
         name: 'Western Wall',
-        id: '',
+        id: '3',
         lat: 31.7767189,
-        lng: 35.2323198
-      },
-      {
+        lng: 35.2323198,
+        category: 'religious'
+      }, {
         name: 'Church of the Holy Sepulchre',
-        id: '',
+        id: '4',
         lat: 31.7784813,
-        lng: 35.2274115
-      },
-      {
+        lng: 35.2274115,
+        category: 'religious'
+      }, {
         name: 'Zion Square',
-        id: '',
+        id: '5',
         lat: 31.7819264,
-        lng: 35.2174048
-      },
-      {
+        lng: 35.2174048,
+        category: 'hangout'
+      }, {
         name: 'Temple Mount',
-        id: '',
+        id: '6',
         lat: 31.7786754,
-        lng: 35.2306103}
-      ],
-      selectValue: ''
+        lng: 35.230610,
+        category: 'religious'
+      }
+    ],
+    selectedCategory: ''
   }
 
+  filterCategory = (category) => {
+    this.setState({selectedCategory : category})
+  }
+
+  filterMarker = () => {}
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <button type="button" id="menu-button"></button>
-          <h1 className="App-title">JLM Hotspots</h1>
-        </header>
-        <Map locations={this.state.locations}/>
-        <div id="sidebar">
-          <div className="search">
-            <select
-                type="text"
-                placeholder="Select category"
-                value=''
-                onChange=''>
-                <option value="secular">Secular</option>
-                <option value="religious">Religious</option>
-                <option value="hangout">Hangout</option>
-              </select>
-          </div>
-          <div className="results">
-            <ol>
-              <li>Mahane Yehuda</li>
-              <li>Blumfield Garden</li>
-              <li>Western Wall</li>
-              <li>Church of the Holy Sepulchre</li>
-              <li>Zion Square</li>
-              <li>Temple Mount</li>
-            </ol>
-          </div>
+    return (<div className="App">
+      <header className="App-header">
+        <button type="button" id="menu-button"></button>
+        <h1 className="App-title">JLM Hotspots</h1>
+      </header>
+      <Map locations={this.state.locations} selectedCategory={this.state.selectedCategory}/>
+      <div id="sidebar">
+        <div className="search">
+          <select type="text" placeholder="Select category" value={this.state.selectedCategory} onChange={(event) => this.filterCategory(event.target.value)}>
+            <option value="secular">Secular</option>
+            <option value="religious">Religious</option>
+            <option value="hangout">Hangout</option>
+          </select>
+        </div>
+        <div className="results">
+          <ol>
+            <li>Mahane Yehuda</li>
+            <li>Blumfield Garden</li>
+            <li>Western Wall</li>
+            <li>Church of the Holy Sepulchre</li>
+            <li>Zion Square</li>
+            <li>Temple Mount</li>
+          </ol>
         </div>
       </div>
-    );
+    </div>);
   }
 }
 
--- src/Map.js
+++ src/Map.js
@@ -1,33 +1,36 @@
-import React, { Component } from 'react';
-import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
+import React, {Component} from 'react';
+import {withGoogleMap, GoogleMap, Marker} from 'react-google-maps';
 
 class Map extends Component {
 
-   render() {
+  render() {
 
-   const JlmMap = withGoogleMap(props => (
-      <GoogleMap
-        defaultCenter = { { lat: 31.7824533, lng: 35.1966413 } }
-        defaultZoom = { 14 }
-      >
-        {this.props.locations.map( location => {
-          return <Marker position={{ lat: location.lat , lng: location.lng}}/>
-        })}
+    let locationsToRender = this.props.locations;
+    console.log(locationsToRender);
 
-      </GoogleMap>
-   ));
+    if (this.props.selectedCategory !== '') {
+      locationsToRender = this.props.locations.filter(location => location.category === this.props.selectedCategory)
+    }
 
-  <JlmMap />
-   return(
-      <div>
-        <JlmMap
-          containerElement={ <div id="map-container"/> }
-          mapElement={ <div id="map" /> }
-        />
-      </div>
-   );
+    const JlmMap = withGoogleMap((props) => (<GoogleMap defaultCenter={{
+        lat: 31.7824533,
+        lng: 35.1966413
+      }} defaultZoom={14}>
+      {
+        locationsToRender.map((location) => {
+          return (<Marker key={location.id} position={{
+              lat: location.lat,
+              lng: location.lng
+            }}/>)
+        })
+      }
+    </GoogleMap>));
 
-   }
+    return (<div>
+      <JlmMap containerElement={<div id = "map-container" />} mapElement={<div id = "map" />}/>
+    </div>);
+
+  }
 };
 
 export default Map;
